fc6d64c6f74967a95bb9335d2fffa04f8625d627
Ducpm <minhducpham.it@gmail.com>
2017-05-18 09:58:29 +0700

    add

---
+++ New folder1/text-slider/js/main.js
@@ -0,0 +1,25 @@
+
+(function($) {
+    "use strict";
+
+    $(document).ready(function() {
+        $(".owl-carousel").each(function(index, el) {
+            var config = $(this).data();
+            config.navText = ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'];
+            config.smartSpeed="800";
+           
+            if($(this).hasClass('owl-style2')){
+                config.animateOut="fadeOut";
+                config.animateIn="fadeIn";    
+            }
+            
+            if($(this).hasClass('dotsData')){
+                 config.dotsData="true";  
+            }
+            $(this).owlCarousel(config);
+        });
+        
+    }); 
+
+
+})(jQuery);
